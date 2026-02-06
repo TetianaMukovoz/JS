407030187ca2c02a89b4a2ad906c864b3379c580
TetianaMukovoz <mukovoztetiana@gmail.com>
2026-02-06 20:20:29 +0200

    Add homework_1

---
+++ homework/HW1/hw_1.js
@@ -0,0 +1,17 @@
+let hello= 'hello';
+let owu='owu';
+let dot_com='.com';
+let ua='ua';
+let number1=1;
+let number2=10;
+let number3=-999;
+let number4=-123;
+let number5=3.14;
+let number6=2.7;
+let number7=16;
+let tr = true;
+let fls= false;
+
+console.log(hello, owu, dot_com, ua, number1, number2, number3, number4, number5, number6, number7,tr,fls);
+
+
---
+++ homework/HW1/hw_2.js
@@ -0,0 +1,6 @@
+let first_name = 'Tania';
+let last_name = 'Muk';
+
+console.log(first_name+' '+last_name);
+//or
+console.log(`${first_name} ${last_name}`)
